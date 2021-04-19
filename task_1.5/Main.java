205eafef8ff8f48d82265a2a489389bde7b7d96d
Alexey-Kondratiev <82752213+Alexey-Kondratiev@users.noreply.github.com>
2021-04-20 02:59:34 +0300

    Update Main.java

--- task_1.5/Main.java
+++ task_1.5/Main.java
@@ -16,7 +16,7 @@ public class Main {
     public static void main(String[] args) {
        Scanner scan=new Scanner(System.in);
        System.out.print("Температура в градусах Цельсия: ");
-       double TC=scan.nextInt();
+       double TC=scan.nextDouble();
        double TF=9.0/5.0*TC+32;
        System.out.println("Температура по Фаренгейту: "+TF);
     }
