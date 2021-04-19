bd0a91efa23026f1e7c928ef46ab9e184e01ed6d
Alexey-Kondratiev <82752213+Alexey-Kondratiev@users.noreply.github.com>
2021-04-19 13:28:51 +0300

    Create Main.java

---
+++ task_1.6/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+/*
+Пользователь вводит число, необходимо увеличить это число на 15% и вывести результат на экран
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        System.out.print("Введите число: ");
+        double a=scan.nextInt();
+        System.out.print(a+"+15% = "+a*1.15);
+    }
+}
