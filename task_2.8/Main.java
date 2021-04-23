f1324f31962220ef920476ce5cbc6f59053a222c
Alexey-Kondratiev <82752213+Alexey-Kondratiev@users.noreply.github.com>
2021-04-23 19:16:11 +0300

    Update Main.java

--- task_2.8/Main.java
+++ task_2.8/Main.java
@@ -2,8 +2,6 @@
 Задание: Дан массив с элементами [2, 3, 4, 5]. С помощью цикла for найдите произведение элементов этого массива.
 */
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         int[] numbers={2,3,4,5};
