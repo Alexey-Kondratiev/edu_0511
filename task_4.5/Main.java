229d08fcf8a9199b1428c8601332df27aeb77205
Alexey-Kondratiev <82752213+Alexey-Kondratiev@users.noreply.github.com>
2021-05-03 18:04:44 +0300

    Create Main.java

---
+++ task_4.5/Main.java
@@ -0,0 +1,18 @@
+package Main.java;
+
+/*
+Создать абстрактный класс Animal с характеристиками животного.
+Создать класс Horse который наследуется от Animal, в классе Horse реализовать метод public void run(){ System.out.println("Игого, я поскакал(а)"); }
+Создать класс Pegasus который наследуется от Horse, в классе Pegasus реализовать метод public void fly(){ System.out.println("Игого, я полетел(а)"); }
+Создать объект лошади и вызвать метод run();
+Создать объект пегаса и вызвать метод fly();
+*/
+
+public class Main{
+    public static void main(String[] args) {
+    Horse mustang=new Horse("Игого",(byte) 4); 
+    Pegasus ares=new Pegasus("Игого", (byte) 4);
+    mustang.run();
+    ares.fly();
+    }
+}
