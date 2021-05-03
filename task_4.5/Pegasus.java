585aa692d68f99e96c279da122e4e1df05ed2591
Alexey-Kondratiev <82752213+Alexey-Kondratiev@users.noreply.github.com>
2021-05-03 20:27:16 +0300

    Update Pegasus.java

--- task_4.5/Pegasus.java
+++ task_4.5/Pegasus.java
@@ -2,7 +2,7 @@ public class Pegasus extends Horse {
     public Pegasus(String speak,byte legs) {
         super(speak,legs);
     }
-//    public void fly() {
- //       System.out.println("Я полетел");
- //   }
+    public void fly() {
+    System.out.println(speak+", я полетел(а)");
+    }
 }
