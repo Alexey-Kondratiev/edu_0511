906b8383f1965f4c3cb75512bbf853907415509e
Alexey-Kondratiev <82752213+Alexey-Kondratiev@users.noreply.github.com>
2021-05-03 18:13:53 +0300

    Create Pegasus.java

---
+++ task_4.5/Pegasus.java
@@ -0,0 +1,8 @@
+public class Pegasus extends Horse {
+    public Pegasus(String speak,byte legs) {
+        super(speak,legs);
+    }
+//    public void fly() {
+ //       System.out.println("Я полетел");
+ //   }
+}
