d0b23557844bc09682cc8ea92510d95274824006
Alexey-Kondratiev <82752213+Alexey-Kondratiev@users.noreply.github.com>
2021-05-03 18:08:53 +0300

    Create Animal.java

---
+++ task_4.5/Animal.java
@@ -0,0 +1,11 @@
+public abstract class Animal {
+    String speak;
+    byte legs;
+    
+    public Animal(String speak,byte legs) {
+        this.speak=speak;
+        this.legs=legs;
+    }
+    String getSpeak() {return this.speak;}
+    byte getLegs() {return  this.legs;}
+}
